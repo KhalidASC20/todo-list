e8bc0b6efe8ffbbeaec15caa5d8f74de5b9e74a5
KhalidASC20 <stewartkhalid5@gmail.com>
2022-10-27 22:52:44 -0700

    add display and delete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
+import TodoList from "./TodoList";
 import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, content: "Beat yo meat" },
+    { id: 2, content: "Go to sleep" },
+  ]);
+
+  function deleteTodo(id) {
+    setTodos((todos) => todos.filter((todos) => todos.id !== id));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>TO-DO LIST</p>
-      </header>
+      <h1>Things To Do</h1>
+      <TodoList todos={todos} deleter={deleteTodo} />
     </div>
   );
 }
